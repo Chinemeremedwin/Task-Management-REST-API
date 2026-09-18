247168088269d2c7bb26363ddf75f887ff4be4e1
Chinemeremedwin <nchinemerem224@gmail.com>
2026-09-18 16:27:13 +0100

    Add task management updates

--- app/database.py
+++ app/database.py
@@ -1,4 +1,5 @@
-from sqlmodel import create_engine, Session
+from sqlmodel import create_engine, Session, SQLModel
+from app.models import Task
 
 DATABASE_URL ="postgresql://postgres:Edwin17@localhost:5432/task_management_db"
 
@@ -7,3 +8,6 @@ engine = create_engine(DATABASE_URL,echo=True)
 def get_session():
     with Session(engine)as session:
         yield session
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI, Depends
 from sqlmodel import Session
-from app.database import get_session
+from app.database import get_session,create_db_and_tables
 
 from app import crud
 
 
 app = FastAPI()
+create_db_and_tables
 
 @app.get("/task")
 def get_task(session: Session = Depends(get_session)): 
